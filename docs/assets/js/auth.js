45e75f4ce1e75f148ba13369639e388f5ca5f787
Masato Hirooka <hi6ma3s1@gmail.com>
2026-01-26 14:43:20 +0900

    mkdocs create

---
+++ docs/assets/js/auth.js
@@ -0,0 +1,16 @@
+﻿(function () {
+  const key = "auth_ok";
+  const pass = "t_ws_doc";
+  const root = document.documentElement;
+
+  if (sessionStorage.getItem(key) === "1") {
+    return;
+  }
+
+  root.style.display = "none";
+  const input = window.prompt("Password:");
+  if (input === pass) {
+    sessionStorage.setItem(key, "1");
+    root.style.display = "";
+  }
+})();
